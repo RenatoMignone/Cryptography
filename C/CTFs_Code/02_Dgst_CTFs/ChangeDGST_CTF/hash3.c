09c02b06086ea67dc0eb722aeae7ca3f844ddf75
renato <renato.mignone@gmail.com>
2025-03-14 17:20:00 +0100

    CTF change dgst

--- C/CTFs_Code/02_Dgst_CTFs/ChangeDGST_CTF/hash3.c
+++ C/CTFs_Code/02_Dgst_CTFs/ChangeDGST_CTF/hash3.c
@@ -1,3 +1,17 @@
+/*
+
+Starting from the file hash3.c, change the code to compute the SHA256.
+
+After having modified it, compute the hash of the modified file (do not add any 
+space, newlines, just do the minimum number of changes),
+
+The flag will be "CRYPTO25{" + hex(SHA256digest(new_file) + "}" 
+where newfile is the hash3.c after the modifications and hex() is the function 
+that represents the binary digest as a string of hex digits.
+
+FLAG = CRYPTO{cf5c2b2a17c55a6986156b993e4ab3f2a261b85dff5591c62e0caaf9e4808975}
+*/
+
 #include <stdio.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
